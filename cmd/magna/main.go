7b55bd26b27dd120eb484ceee6909abb65791e2f
Rob Shakir <robjs@google.com>
2022-08-27 16:15:54 -0700

    Check in initial binary for magna.
     * (A) cmd/magna
       - Add a binary that pulls the various parts of magna togetehr.

---
+++ cmd/magna/main.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/go-ping/ping"
+	"github.com/open-traffic-generator/snappi/gosnappi/otg"
+	"github.com/openconfig/magna/flows/mpls"
+	"github.com/openconfig/magna/lwotg"
+	"github.com/openconfig/magna/lwotgtelem"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/reflection"
+	"k8s.io/klog"
+
+	gpb "github.com/openconfig/gnmi/proto/gnmi"
+)
+
+func main() {
+	// Since we have a mix of glog and klog we have flags that overlap - and hence
+	// need to override this.
+	if flag.CommandLine.Lookup("log_dir") != nil {
+		flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+	}
+	klog.InitFlags(nil)
+
+	var (
+		telemPort, port   string
+		certFile, keyFile string
+		targetName        string
+	)
+
+	flag.StringVar(&port, "port", os.Getenv("PORT"), "Port for OTG server.")
+	flag.StringVar(&telemPort, "telemetry_port", os.Getenv("TELEMETRY_PORT"), "Port for gNMI server.")
+	flag.StringVar(&certFile, "certfile", "", "Certificate file for gNMI.")
+	flag.StringVar(&keyFile, "keyfile", "", "Key file for gNMI.")
+	flag.StringVar(&targetName, "target", "ate", "Name for the gNMI target exposed.")
+	flag.Parse()
+
+	otgSrv := lwotg.New()
+	telemSrv, err := lwotgtelem.New(context.Background(), targetName)
+	if err != nil {
+		klog.Exitf("cannot set up telemetry server, %v", err)
+	}
+
+	fh, task, err := mpls.New()
+	if err != nil {
+		klog.Exitf("cannot initialise MPLS flow handler, %v", err)
+	}
+
+	otgSrv.AddFlowHandlers(fh)
+	telemSrv.AddTask(task)
+
+	hintCh := make(chan lwotg.Hint, 100)
+	otgSrv.SetHintChannel(hintCh)
+	otgSrv.SetProtocolHandler(gatewayPinger)
+	telemSrv.SetHintChannel(context.Background(), hintCh)
+
+	otgLis, err := net.Listen("tcp", fmt.Sprintf(":%s", port))
+	if err != nil {
+		klog.Exitf("cannot listen on port %d, err: %v", port, err)
+	}
+
+	gnmiLis, err := net.Listen("tcp", fmt.Sprintf(":%s", telemPort))
+	if err != nil {
+		klog.Exitf("cannot listen on port %d, err: %v", telemPort, err)
+	}
+
+	otgS := grpc.NewServer(grpc.Creds(insecure.NewCredentials()))
+	reflection.Register(otgS)
+	otg.RegisterOpenapiServer(otgS, otgSrv)
+
+	creds, err := credentials.NewServerTLSFromFile(certFile, keyFile)
+	if err != nil {
+		klog.Exitf("cannot create gNMI credentials, %v", err)
+	}
+
+	gnmiS := grpc.NewServer(grpc.Creds(creds))
+	reflection.Register(gnmiS)
+	gpb.RegisterGNMIServer(gnmiS, telemSrv.GNMIServer)
+
+	klog.Infof("OTG listening at %s", otgLis.Addr())
+	klog.Infof("gNMI listening at %s", gnmiLis.Addr())
+	go otgS.Serve(otgLis)
+	go gnmiS.Serve(gnmiLis)
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL, os.Interrupt)
+	sig := <-sigs
+	klog.Infof("Received signal %v", sig)
+	os.Exit(1)
+}
+
+// gatewayPinger is a function that pings all gateway addresses when start protocols is called.
+func gatewayPinger(cfg *otg.Config, _ otg.ProtocolState_State_Enum) error {
+	gw := []string{}
+	for _, d := range cfg.GetDevices() {
+		for _, i := range d.GetEthernets() {
+			for _, a4 := range i.GetIpv4Addresses() {
+				gw = append(gw, a4.GetGateway())
+			}
+		}
+	}
+	klog.Infof("ping gateways %v", gw)
+
+	for _, a := range gw {
+		pinger, err := ping.NewPinger(a)
+		if err != nil {
+			return fmt.Errorf("cannot parse gateway address %s, %v", a, err)
+		}
+		pinger.SetPrivileged(true)
+		pinger.Count = 1
+		if err := pinger.Run(); err != nil {
+			return fmt.Errorf("cannot ping address %s, %v", a, err)
+		}
+		klog.Infof("ping statistics, %v", pinger.Statistics())
+	}
+	return nil
+}
--- go.mod
+++ go.mod
@@ -15,14 +15,17 @@ require (
 )
 
 require (
+	github.com/go-ping/ping v1.1.0 // indirect
 	github.com/golang/glog v1.0.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/gopacket v1.1.19 // indirect
+	github.com/google/uuid v1.2.0 // indirect
 	github.com/kylelemons/godebug v1.1.0 // indirect
 	github.com/openconfig/goyang v1.1.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/vishvananda/netns v0.0.0-20191106174202-0a2b9b5464df // indirect
 	golang.org/x/net v0.0.0-20220812174116-3211cb980234 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	golang.org/x/sys v0.0.0-20220818161305-2296e01440c6 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
