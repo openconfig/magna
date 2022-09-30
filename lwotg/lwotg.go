66cb43c869bb47fde097e6e65063e755c9d93859
Rob Shakir <robjs@google.com>
2022-09-30 07:36:13 -0700

    Add handler for SetProtocolState. (#7)
    * Add ARPSubscribe mechanism.
    
     * (M) intf/accessor_linux.go
      - Add a mechanism to subscribe to ARP entries that are populated
        in the kernel.
     * (M) intf/intf(_test)?.go
      - Add mechanism that subscribes to the ARP table independently
        of the underlying implementation.
    
    * Add initial telemetry server implementation.
    
     * (A) lwotgtelem/*
       - Add a gNMI server using lemming's gnmit that can host tasks
         that populate OTG telemetry.
     * (A) lwotg/*
       - Add initial file for lwotg which solely defines a Hint type.
    
    * Add handler for SetProtocolState.
    
     * (M) lwotg/*
       - Add support for SetProtocolState which calls a handler function.
    
    * Add OTG configuration parser for interface IPv4 addresses.
    
     * (A) lwotg/intf(_test)?.go
       - Add a parser that takes an OTG configuration and maps it to
         an underlying set of configurable objects on a host system.

--- lwotg/lwotg.go
+++ lwotg/lwotg.go
@@ -3,6 +3,18 @@
 // often abbreviated to OTG.
 package lwotg
 
+import (
+	"context"
+	"net"
+	"sync"
+
+	"github.com/open-traffic-generator/snappi/gosnappi/otg"
+	"github.com/openconfig/magna/intf"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"k8s.io/klog"
+)
+
 // Hint is <group, key, value> tuple that can be handed to modules of the
 // OTG implementation to perform their functions. For example, it may be
 // used to communicate mappings between system interfaces and the names that
@@ -15,4 +27,138 @@ type Hint struct {
 	Key string
 	// Value is the value stored for the hint.
 	Value string
+}
+
+const (
+	// InterfaceHintGroupName is the name of the group that is used to
+	// handle interface details.
+	InterfaceHintGroupName string = "interface_map"
+)
+
+// Server implements the OTG ("Openapi") server.
+type Server struct {
+	*otg.UnimplementedOpenapiServer
+
+	// hintCh is the a channel that is used to send hints about the configuration
+	// to other elements of the OTG system. Particularly, it is used to send hints
+	// to the telemetry mapping functions.
+	hintCh chan Hint
+
+	// protocolHandler is a function called when the OTG SetProtocolState RPC
+	// is called. It is used to ensure that anything that needs to be done in
+	// the underlying system is performed (e.g., ensuring ARP is populated, or
+	// other daemons are started).
+	protocolHandler func(*otg.Config, otg.ProtocolState_State_Enum) error
+
+	// chMu protects the configHandlers slice.
+	chMu sync.Mutex
+	// configHandlers is a slice of functions that are called when the SetConfig
+	// RPC is called. Each handles the configuration and returns an error if it
+	// finds one. It is possible that multiple handlers read the same configuration
+	// based on the registered handlers.
+	configHandlers []func(*otg.Config) error
+
+	// TODO(robjs): Add support for:
+	//   - functions that handle protocol configuration.
+	//   - functions that handle flow configuration.
+	//   - functions that handle traffic generation.
+
+	// cfg is a cache of the current OTG configuration.
+	cfg *otg.Config
+}
+
+// New returns a new lightweight OTG (LWOTG) server.
+func New() *Server {
+	s := &Server{
+		configHandlers: []func(*otg.Config) error{},
+	}
+
+	s.AddConfigHandler(s.baseInterfaceHandler)
+	return s
+}
+
+// AddConfigHandler adds the specified fn to the set of config handlers.
+func (s *Server) AddConfigHandler(fn func(*otg.Config) error) {
+	s.chMu.Lock()
+	defer s.chMu.Unlock()
+	s.configHandlers = append(s.configHandlers, fn)
+}
+
+// SetHintChannel sets the hint channel to the specified channel.
+func (s *Server) SetHintChannel(ch chan Hint) {
+	s.hintCh = ch
+}
+
+// SetProtocolHandler sets the specified function as the function to be called when the
+// SetPrrotocolState RPC is called.
+func (s *Server) SetProtocolHandler(fn func(*otg.Config, otg.ProtocolState_State_Enum) error) {
+	s.protocolHandler = fn
+}
+
+// SetProtocolState handles the SetProtocolState OTG call. In this implementation it calls the
+// protocolHandler function that has been specified.
+func (s *Server) SetProtocolState(ctx context.Context, req *otg.SetProtocolStateRequest) (*otg.SetProtocolStateResponse, error) {
+	klog.Infof("Setting protocol state based on request, %s", req)
+	if s.protocolHandler != nil {
+		if err := s.protocolHandler(s.cfg, req.GetProtocolState().GetState()); err != nil {
+			return nil, err
+		}
+	}
+	return &otg.SetProtocolStateResponse{StatusCode_200: &otg.ResponseWarning{}}, nil
+}
+
+// SetConfig handles the SetConfig OTG RPC. In this implementation it calls the set
+// of configHandlers that have been registered with the server.
+func (s *Server) SetConfig(ctx context.Context, req *otg.SetConfigRequest) (*otg.SetConfigResponse, error) {
+	if req.Config == nil {
+		return nil, status.Errorf(codes.InvalidArgument, "invalid request configuration received, %v", req)
+	}
+
+	for _, fn := range s.configHandlers {
+		if err := fn(req.Config); err != nil {
+			return nil, err
+		}
+	}
+
+	s.cfg = req.Config
+
+	// TODO(robjs): remove this status 200 once OTG has been updated.
+	return &otg.SetConfigResponse{StatusCode_200: &otg.ResponseWarning{}}, nil
+}
+
+// baseInterfaceHandler is a built-in handler for interface configuration which is used as a configHandler.
+func (s *Server) baseInterfaceHandler(cfg *otg.Config) error {
+	intfs, err := portsToSystem(cfg.Ports, cfg.Devices)
+	if err != nil {
+		return err
+	}
+
+	if s.hintCh != nil {
+		for _, i := range intfs {
+			h := Hint{
+				Group: InterfaceHintGroupName,
+				Key:   i.SystemName,
+				Value: i.OTGEthernetName,
+			}
+			select {
+			case s.hintCh <- h:
+			default:
+				// Non-blocking if the channel is full.
+			}
+		}
+	}
+
+	for _, i := range intfs {
+		for _, a := range i.IPv4 {
+			n := &net.IPNet{
+				IP:   a.Address,
+				Mask: net.CIDRMask(a.Mask, 32),
+			}
+			klog.Infof("Configuring interface %s with address %s", i.SystemName, n)
+			if err := intf.AddIP(i.SystemName, n); err != nil {
+				return status.Errorf(codes.Internal, "cannot configure address %s on interface %s, err: %v", n, i.SystemName, err)
+			}
+		}
+	}
+	return nil
 }
---
+++ lwotg/lwotg_test.go
@@ -0,0 +1,98 @@
+package lwotg
+
+import (
+	"context"
+	"net"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/open-traffic-generator/snappi/gosnappi/otg"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/protobuf/testing/protocmp"
+)
+
+func TestSetProtocols(t *testing.T) {
+
+	okResponse := &otg.SetProtocolStateResponse{
+		StatusCode_200: &otg.ResponseWarning{},
+	}
+
+	state := "UNKNOWN"
+
+	tests := []struct {
+		desc              string
+		inProtocolHandler func(*otg.Config, otg.ProtocolState_State_Enum) error
+		inRequest         *otg.SetProtocolStateRequest
+		wantResponse      *otg.SetProtocolStateResponse
+		wantFn            func(t *testing.T)
+	}{{
+		desc: "start with nil protocol handler",
+		inRequest: &otg.SetProtocolStateRequest{
+			ProtocolState: &otg.ProtocolState{
+				State: otg.ProtocolState_State_start,
+			},
+		},
+		wantResponse: okResponse,
+	}, {
+		desc: "protocol handler called",
+		inProtocolHandler: func(_ *otg.Config, r otg.ProtocolState_State_Enum) error {
+			switch r {
+			case otg.ProtocolState_State_start:
+				state = "START"
+			case otg.ProtocolState_State_stop:
+				state = "STOP"
+			}
+			return nil
+		},
+		inRequest: &otg.SetProtocolStateRequest{
+			ProtocolState: &otg.ProtocolState{
+				State: otg.ProtocolState_State_start,
+			},
+		},
+		wantResponse: okResponse,
+		wantFn: func(t *testing.T) {
+			t.Helper()
+			if state != "START" {
+				t.Fatalf("did not get expected state, got: %s, want: START", state)
+			}
+		},
+	}}
+
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			lw := New()
+			lw.SetProtocolHandler(tt.inProtocolHandler)
+
+			l, err := net.Listen("tcp", "localhost:0")
+			if err != nil {
+				t.Fatalf("cannot listen, %v", err)
+			}
+			t.Cleanup(func() { l.Close() })
+
+			s := grpc.NewServer(grpc.Creds(insecure.NewCredentials()))
+			otg.RegisterOpenapiServer(s, lw)
+			go s.Serve(l)
+			t.Cleanup(s.Stop)
+
+			conn, err := grpc.Dial(l.Addr().String(), grpc.WithTransportCredentials(insecure.NewCredentials()))
+			if err != nil {
+				t.Fatalf("cannot dial server %s, err: %v", l.Addr().String(), err)
+			}
+			c := otg.NewOpenapiClient(conn)
+
+			got, err := c.SetProtocolState(context.Background(), tt.inRequest)
+			if err != nil {
+				t.Fatalf("got error sending request (%s), err: %v", tt.inRequest, err)
+			}
+
+			if diff := cmp.Diff(got, tt.wantResponse, protocmp.Transform()); diff != "" {
+				t.Fatalf("did not get expected result, diff(-got,+want):\n%s", diff)
+			}
+
+			if tt.wantFn != nil {
+				tt.wantFn(t)
+			}
+		})
+	}
+}
