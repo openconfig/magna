2cd3278a79bf5b38451e4871926c74569d3f1229
Rob Shakir <robjs@google.com>
2023-06-12 14:35:37 -0700

    Add support for telemetry to be generated by a flow.
     * (M) flow/mpls/mpls(_test)?.go
      - Add telemetry reporting for a flow.

--- flows/mpls/mpls.go
+++ flows/mpls/mpls.go
@@ -5,8 +5,11 @@
 package mpls
 
 import (
+	"encoding/binary"
 	"fmt"
+	"math"
 	"net"
+	"sort"
 	"sync"
 	"time"
 
@@ -158,6 +161,7 @@ func New() (lwotg.FlowGeneratorFn, gnmit.Task, error) {
 			return nil, false, fmt.Errorf("cannot determine ports, %v", err)
 		}
 
+		f.Name = &val{s: flow.Name, ts: flowTimeFn()}
 		klog.Infof("generating flow %s: tx: %s, rx: %s, rate: %d pps", flow.GetName(), tx, rx, pps)
 
 		genFunc := func(stop chan struct{}) {
@@ -191,7 +195,7 @@ func New() (lwotg.FlowGeneratorFn, gnmit.Task, error) {
 						}
 					}
 
-					f.updateTx(pps, size)
+					f.updateTx(int(pps), size)
 					// TODO(robjs): This assumes that sending the packets take zero time. We should consider being more accurate here.
 					time.Sleep(1 * time.Second)
 				}
@@ -215,7 +219,7 @@ func New() (lwotg.FlowGeneratorFn, gnmit.Task, error) {
 					klog.Infof("MPLSFlowHandler Rx exiting on %s", rx)
 					return
 				case p := <-packetCh:
-					if err := rxPacket(gnmiCh, p); err != nil {
+					if err := rxPacket(f, p); err != nil {
 						klog.Errorf("MPLSFlowHandler cannot receive packet on interface %s, %v", rx, err)
 						return
 					}
@@ -235,15 +239,17 @@ func New() (lwotg.FlowGeneratorFn, gnmit.Task, error) {
 // flowCounters is an internal data store for parameters that are sent as part of the
 // OTG telemetry for the flow.
 type flowCounters struct {
+	// Name is the name of the flow.
+	Name *val
 	// tx and rx store the counters for statistics relating to the flow.
 	Tx, Rx *stats
 
-	mu sync.Mutex
+	mu sync.RWMutex
 	// transmit indicates whether the flow is currently transmitting.
 	Transmit *val
 
 	// tsMu protects the Timeseries map.
-	tsMu sync.Mutex
+	tsMu sync.RWMutex
 	// Timeseries maps a unix timestamp (in seconds) to an integer number of packets
 	// received in that interval for rate calculation.
 	Timeseries map[int64]int
@@ -267,24 +273,24 @@ func newFlowCounters() *flowCounters {
 
 // updateTx updates the transmit counters for the flow according to the specified
 // packets per second rate and packet size.
-func (f *flowCounters) updateTx(pps int64, size int) {
+func (f *flowCounters) updateTx(pps, size int) {
 	f.Tx.mu.Lock()
 	defer f.Tx.mu.Unlock()
 
 	now := flowTimeFn()
 
-	f.Tx.Rate = &val{ts: now, f: float64(pps)}
+	f.Tx.Rate = &val{ts: now, f: float32(pps)}
 
 	if f.Tx.Octets == nil {
 		f.Tx.Octets = &val{}
 	}
-	f.Tx.Octets.i += pps * int64(size)
+	f.Tx.Octets.u += uint64(pps) * uint64(size)
 	f.Tx.Octets.ts = now
 
 	if f.Tx.Pkts == nil {
 		f.Tx.Pkts = &val{}
 	}
-	f.Tx.Pkts.i += pps
+	f.Tx.Pkts.u += uint64(pps)
 	f.Tx.Pkts.ts = now
 }
 
@@ -303,13 +309,13 @@ func (f *flowCounters) updateRx(ts time.Time, size int) {
 	if f.Rx.Octets == nil {
 		f.Rx.Octets = &val{}
 	}
-	f.Rx.Octets.i += int64(size)
+	f.Rx.Octets.u += uint64(size)
 	f.Rx.Octets.ts = ts.UnixNano()
 
 	if f.Rx.Pkts == nil {
 		f.Rx.Pkts = &val{}
 	}
-	f.Rx.Pkts.i += 1
+	f.Rx.Pkts.u += 1
 	f.Rx.Pkts.ts = ts.UnixNano()
 }
 
@@ -320,6 +326,7 @@ func (f *flowCounters) setTransmit(state bool) {
 	f.Transmit.ts = time.Now().UnixNano()
 }
 
+// lossPct calculates the percentage loss that the flow has experienced.
 func (f *flowCounters) lossPct() float32 {
 	f.Tx.mu.Lock()
 	defer f.Tx.mu.Unlock()
@@ -327,30 +334,169 @@ func (f *flowCounters) lossPct() float32 {
 	f.Rx.mu.Lock()
 	defer f.Rx.mu.Unlock()
 
-	return float32(f.Rx.Pkts.i/f.Tx.Pkts.i) * 100.0
+	return float32(f.Tx.Pkts.u-f.Rx.Pkts.u) / float32(f.Tx.Pkts.u) * 100.0
 }
 
-func (f *flowCounters) telemetry(name string) []*gpb.Notification {
+const (
+	// slidingWindow specifies the number of seconds over which to calculate the rate
+	// of the flow.
+	slidingWindow int = 5
+)
+
+// currentRate calculates the current received rate based on the sliding window
+// size specified. It returns the rate it bits per second.
+func (f *flowCounters) rxRate() float32 {
+	f.tsMu.Lock()
+	defer f.tsMu.Unlock()
+	keys := []int{}
+	for k := range f.Timeseries {
+		keys = append(keys, int(k))
+	}
+	sort.Ints(keys)
+
+	if len(keys) == 0 || len(keys) == 1 {
+		// If we do not have enough datapoints to calculate the rate then return 0.0
+		return 0.0
+	}
+
+	firstEntry := 0
+	if len(keys) > slidingWindow {
+		firstEntry = len(keys) - slidingWindow - 1
+	}
+
+	var sum int
+	// Ignore the last slot since we may still be appending to it.
+	for i := firstEntry; i < len(keys)-1; i++ {
+		sum += f.Timeseries[int64(keys[i])]
+	}
+	// calculate the time delta over which these entries were calculated.
+	delta := float32(keys[int64(len(keys)-1)]) - float32(keys[firstEntry])
+
+	// Average the rate and return in bits per second rather than bytes.
+	return float32(sum) / delta * 8.0
+}
+
+// telemetry generates the set of gNMI Notifications that describes the flow's current state.
+func (f *flowCounters) telemetry() []*gpb.Notification {
 	type datapoint struct {
-		d *otgyang.Device
+		d  *otgyang.Device
 		ts int64
 	}
-	
+
+	// Cannot generate statistics until the flow is initialised with a name.
+	if f.Name == nil {
+		return nil
+	}
+	name := f.Name.s
+
 	upd := []*datapoint{}
 
-	t := &otyang.Device{}.GetOrCreateFlow(name)
-	t.Transmit = ygot.Bool(f.Transmit.b)
-	upd = append(upd, &datapoint{d: t, ts: f.Transmit.ts})
+	if f.Transmit != nil {
+		t := &otgyang.Device{}
+		t.GetOrCreateFlow(name).Transmit = ygot.Bool(f.Transmit.b)
+		upd = append(upd, &datapoint{d: t, ts: f.Transmit.ts})
+	}
 
-	l := &otgyang.Device{}.GetOrCreateFlow(name)
-	l.LossPct = f.lossPct()
-	upd = append(upd, 
+	if f.Tx != nil && f.Rx != nil {
+		if f.Tx.Pkts != nil && f.Rx.Pkts != nil {
+			l := &otgyang.Device{}
+			l.GetOrCreateFlow(name).LossPct = float32ToBinary(f.lossPct())
+			upd = append(upd, &datapoint{d: l, ts: flowTimeFn()})
+		}
+	}
 
+	if f.Tx != nil {
+		f.Tx.mu.RLock()
+		if f.Tx.Octets != nil {
+			// TX statistics
+			txo := &otgyang.Device{}
+			txo.GetOrCreateFlow(name).GetOrCreateCounters().OutOctets = ygot.Uint64(f.Tx.Octets.u)
+			upd = append(upd, &datapoint{d: txo, ts: f.Tx.Octets.ts})
+		}
+
+		if f.Tx.Pkts != nil {
+			tp := &otgyang.Device{}
+			tp.GetOrCreateFlow(name).GetOrCreateCounters().OutPkts = ygot.Uint64(f.Tx.Pkts.u)
+			upd = append(upd, &datapoint{d: tp, ts: f.Tx.Pkts.ts})
+		}
+
+		if f.Tx.Rate != nil {
+			tr := &otgyang.Device{}
+			tr.GetOrCreateFlow(name).OutRate = float32ToBinary(f.Tx.Rate.f)
+			upd = append(upd, &datapoint{d: tr, ts: f.Tx.Rate.ts})
+		}
+		f.Tx.mu.RUnlock()
+	}
+
+	if f.Rx != nil {
+		// RX statistics
+		f.Rx.mu.RLock()
+
+		if f.Rx.Octets != nil {
+			r := &otgyang.Device{}
+			r.GetOrCreateFlow(name).GetOrCreateCounters().InOctets = ygot.Uint64(f.Rx.Octets.u)
+			upd = append(upd, &datapoint{d: r, ts: f.Rx.Octets.ts})
+		}
+
+		if f.Rx.Pkts != nil {
+			rp := &otgyang.Device{}
+			rp.GetOrCreateFlow(name).GetOrCreateCounters().InPkts = ygot.Uint64(f.Rx.Pkts.u)
+			upd = append(upd, &datapoint{d: rp, ts: f.Rx.Pkts.ts})
+		}
+		f.Rx.mu.RUnlock()
+
+		rr := &otgyang.Device{}
+		rr.GetOrCreateFlow(name).InRate = float32ToBinary(f.rxRate()) // express in bits per second rather than bytes
+		upd = append(upd, &datapoint{d: rr, ts: flowTimeFn()})
+	}
+
+	notis := []*gpb.Notification{}
+	for _, u := range upd {
+		notifications, err := ygot.TogNMINotifications(u.d, u.ts, ygot.GNMINotificationsConfig{UsePathElem: true})
+		if err != nil {
+			klog.Errorf("cannot render stats to notification, input: %v, err: %v", u, err)
+			continue
+		}
+
+		for _, n := range notifications {
+			// TODO(robjs): This is a hack, we need to remove the additional flow name updates. ygot.TogNMINotifications
+			// needs a path filter to avoid duplicating leaves, or a way to store timestamps per-leaf. Length 3 means we
+			// the name, state/name, and then one other leaf.
+			if len(n.Update) == 3 {
+				nn := &gpb.Notification{Timestamp: n.Timestamp}
+				for _, u := range n.Update {
+					if len(u.Path.Elem) == 3 || len(u.Path.Elem) == 4 {
+						if u.Path.Elem[len(u.Path.Elem)-1].Name == "name" {
+							continue
+						}
+					}
+					nn.Update = append(nn.Update, u)
+				}
+				notis = append(notis, nn)
+			}
+		}
+	}
+
+	nameUpd := &otgyang.Device{}
+	nameUpd.GetOrCreateFlow(name)
+	n, err := ygot.TogNMINotifications(nameUpd, f.Name.ts, ygot.GNMINotificationsConfig{UsePathElem: true})
+	if err != nil {
+		klog.Errorf("cannot render name notification, got err: %v", err)
+	}
+	notis = append(notis, n...)
+
+	return notis
+}
+
+func float32ToBinary(f float32) otgyang.Binary {
+	b := make([]byte, 4)
+	binary.LittleEndian.PutUint32(b, math.Float32bits(f))
+	return otgyang.Binary(b)
 }
 
 // stats stores metrics that are tracked for an individual flow direction.
 type stats struct {
-	mu sync.Mutex
+	mu sync.RWMutex
 	// rate indicates the rate at which packets are being sent or received according
 	// to the specific context.
 	Rate *val
@@ -364,16 +510,18 @@ type val struct {
 	// ts is the timestamp in nanoseconds since the unix epoch that the value was
 	// collected.
 	ts int64
-	// f is the value if it is of type float64.
-	f float64
-	// i is the value if it is of type int64.
-	i int64
+	// f is the value if it is of type float32.
+	f float32
+	// u is the value if it is of type uint64.
+	u uint64
 	// b is the value if it is of type bool.
 	b bool
+	// s is the value if it is of type string.
+	s string
 }
 
 // rxPacket is called for each packet that is received.
-func rxPacket(gnmiCh chan *gpb.Notification, p gopacket.Packet) error {
+func rxPacket(f *flowCounters, p gopacket.Packet) error {
 	// TODO(robjs): filter packets to ensure that they are packets that are part of this flow.
 
 	// TODO(robjs): every time tick, generate a notification from the stats that are
