9522da02ca7cc053a6f11f9cb7a3205d014df585
Rob Shakir <robjs@google.com>
2023-06-30 01:38:44 +0000

    Refactor mirror service into a gRPC microservice.
     * (A) mirrorsrv/mirror(_test)?.go
       - small microservice that can be called from ONDATRA tests to
         allow for new mirroring sessions to be started and stopped.
     * (M) proto/mirror/...
       - add proto service definition.

---
+++ mirrorsrv/mirror.go
@@ -0,0 +1,168 @@
+package mirrorsrv
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"github.com/google/gopacket/pcap"
+	mpb "github.com/openconfig/magna/proto/mirror"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"k8s.io/klog"
+)
+
+type Server struct {
+	*mpb.UnimplementedMirrorServer
+
+	stopMu  sync.Mutex
+	stopChs map[string]chan struct{}
+}
+
+func New() *Server {
+	return &Server{
+		stopChs: map[string]chan struct{}{},
+	}
+}
+
+var (
+	// copyFunc can be overloaded during unit testing to avoid the need to have
+	// physical interfaces available to test again.
+	copyFunc = copyPackets
+	// filterFunc can be overloaded during unit testing to avoid the need to
+	// fake packets.
+	filterFunc = mplsFilter
+)
+
+var (
+	// pcapTimeout specifies the timeout for packet captures to be established.
+	pcapTimeout = 30 * time.Second
+)
+
+const (
+	// packetSize specifies the number of bytes that are to be read from the wire.
+	packetSize = 9000
+)
+
+// mplsFilter returns true if a packet is an MPLS unicast packet.
+func mplsFilter(p gopacket.Packet) bool {
+	e, ok := p.Layer(layers.LayerTypeEthernet).(*layers.Ethernet)
+	if !ok {
+		return false
+	}
+	if e.EthernetType != layers.EthernetTypeMPLSUnicast {
+		return false
+	}
+	klog.Infof("copying packet, %v", p)
+	return true
+}
+
+// copyPackets returns a function that copies packets that are received on the 'from' interface
+// to the 'to' interface. The packets match the specified filter function.
+func copyPackets(from, to string, filter func(p gopacket.Packet) bool) func(chan struct{}) {
+	return func(stop chan struct{}) {
+		klog.Infof("launching goroutine to copy from %s->%s", from, to)
+		ih, err := pcap.NewInactiveHandle(from)
+		if err != nil {
+			klog.Errorf("cannot open interface %s for reading, %v", from, err)
+			return
+		}
+		if err := ih.SetImmediateMode(true); err != nil {
+			klog.Errorf("cannot set immediate mode for interface %s, %v", from, err)
+			return
+		}
+		if err := ih.SetPromisc(true); err != nil {
+			klog.Errorf("cannot set promiscuous mode for interface %s, %v", from, err)
+			return
+		}
+		if err := ih.SetSnapLen(packetSize); err != nil {
+			klog.Errorf("cannot set capture length to %d for interface %s, %v", packetSize, from, err)
+			return
+		}
+		rx, err := ih.Activate()
+		if err != nil {
+			klog.Errorf("cannot open rx interface %s, %v", from, err)
+			return
+		}
+		defer rx.Close()
+
+		tx, err := pcap.OpenLive(to, packetSize, true, pcapTimeout)
+		if err != nil {
+			klog.Errorf("cannot open tx interface %s, %v", to, err)
+			return
+		}
+		defer tx.Close()
+
+		ps := gopacket.NewPacketSource(rx, rx.LinkType())
+		for {
+			select {
+			case <-stop:
+				return
+			case p := <-ps.Packets():
+				if !filter(p) {
+					continue
+				}
+
+				if err := tx.WritePacketData(p.Data()); err != nil {
+					klog.Errorf("cannot write packet to tx interface %s, err: %v", to, err)
+					return
+				}
+			}
+		}
+	}
+}
+
+func key(from, to string) string {
+	return fmt.Sprintf("%s:%s", from, to)
+}
+
+func (s *Server) sessionExists(from, to string) bool {
+	s.stopMu.Lock()
+	defer s.stopMu.Unlock()
+	if _, ok := s.stopChs[key(from, to)]; ok {
+		return true
+	}
+	return false
+}
+
+func (s *Server) addSession(from, to string, c chan struct{}) {
+	s.stopMu.Lock()
+	defer s.stopMu.Unlock()
+	s.stopChs[key(from, to)] = c
+}
+
+func (s *Server) stopSession(from, to string) {
+	s.stopMu.Lock()
+	defer s.stopMu.Unlock()
+	s.stopChs[key(from, to)] <- struct{}{}
+	delete(s.stopChs, key(from, to))
+}
+
+func (s *Server) Start(ctx context.Context, req *mpb.StartRequest) (*mpb.StartResponse, error) {
+	if req.From == "" || req.To == "" {
+		return nil, status.Errorf(codes.InvalidArgument, "unspecified from or to in request, got: %s", req)
+	}
+	if s.sessionExists(req.From, req.To) {
+		return nil, status.Errorf(codes.AlreadyExists, "session between %s and %s already exists", req.From, req.To)
+	}
+
+	fn := copyFunc(req.From, req.To, filterFunc)
+	stop := make(chan struct{})
+	s.addSession(req.From, req.To, stop)
+	go fn(stop)
+	return &mpb.StartResponse{}, nil
+}
+
+func (s *Server) Stop(ctx context.Context, req *mpb.StopRequest) (*mpb.StopResponse, error) {
+	if req.From == "" || req.To == "" {
+		return nil, status.Errorf(codes.InvalidArgument, "unspecified from or to in request, got: %s", req)
+	}
+	if !s.sessionExists(req.From, req.To) {
+		return nil, status.Errorf(codes.NotFound, "cannot find session between %s and %s", req.From, req.To)
+	}
+	s.stopSession(req.From, req.To)
+	return &mpb.StopResponse{}, nil
+}
---
+++ mirrorsrv/mirror_test.go
@@ -0,0 +1,197 @@
+package mirrorsrv
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"reflect"
+	"sync"
+	"testing"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
+
+	"github.com/google/gopacket"
+	mpb "github.com/openconfig/magna/proto/mirror"
+)
+
+func startServer(t *testing.T) (*Server, string) {
+	t.Helper()
+	srv := grpc.NewServer()
+	ms := New()
+	mpb.RegisterMirrorServer(srv, ms)
+
+	lis, err := net.Listen("tcp", "localhost:0")
+	if err != nil {
+		t.Fatalf("cannot start server, %v", err)
+	}
+	t.Cleanup(func() { lis.Close() })
+	go srv.Serve(lis)
+	t.Cleanup(srv.Stop)
+	return ms, lis.Addr().String()
+}
+
+func TestStart(t *testing.T) {
+
+	var called []string
+	tests := []struct {
+		desc       string
+		inReq      *mpb.StartRequest
+		inSessions map[string]chan struct{}
+		inFunc     func(string, string, func(p gopacket.Packet) bool) func(chan struct{})
+		wantCode   codes.Code
+		wantCalled []string
+	}{{
+		desc:     "empty input",
+		inReq:    &mpb.StartRequest{},
+		wantCode: codes.InvalidArgument,
+	}, {
+		desc:     "no from",
+		inReq:    &mpb.StartRequest{To: "eth2"},
+		wantCode: codes.InvalidArgument,
+	}, {
+		desc:     "no to",
+		inReq:    &mpb.StartRequest{From: "eth3"},
+		wantCode: codes.InvalidArgument,
+	}, {
+		desc:       "existing session",
+		inReq:      &mpb.StartRequest{From: "eth1", To: "eth2"},
+		inSessions: map[string]chan struct{}{"eth1:eth2": make(chan struct{})},
+		wantCode:   codes.AlreadyExists,
+	}, {
+		desc:  "create new session",
+		inReq: &mpb.StartRequest{From: "eth1", To: "eth2"},
+		inFunc: func(from, to string, _ func(p gopacket.Packet) bool) func(chan struct{}) {
+			called = append(called, fmt.Sprintf("%s:%s", from, to))
+			return func(_ chan struct{}) {}
+		},
+		wantCalled: []string{"eth1:eth2"},
+	}}
+
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+
+			if tt.inFunc != nil {
+				copyFunc = tt.inFunc
+				t.Cleanup(func() { copyFunc = copyPackets })
+			}
+
+			ms, addr := startServer(t)
+			conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+			if err != nil {
+				t.Fatalf("got error in grpc.Dial(%v), got err: %v", addr, err)
+			}
+
+			if tt.inSessions != nil {
+				ms.stopChs = tt.inSessions
+			}
+
+			client := mpb.NewMirrorClient(conn)
+			got, err := client.Start(context.Background(), tt.inReq)
+			t.Logf("got: %s %v", got, err)
+			if err != nil {
+				s, ok := status.FromError(err)
+				if !ok {
+					t.Fatalf("did not get correct return type, %v", err)
+				}
+				if s.Code() != tt.wantCode {
+					t.Fatalf("did not get expected error code, got: %v, want: %v", s.Code(), tt.wantCode)
+				}
+			}
+
+			if tt.inFunc != nil {
+				t.Logf("got called value: %v", called)
+				if !reflect.DeepEqual(tt.wantCalled, called) {
+					t.Fatalf("did not get expected called, got: %v, want: %v", called, tt.wantCalled)
+				}
+			}
+			called = []string{}
+		})
+	}
+}
+
+func TestStop(t *testing.T) {
+	called := false
+	var wg sync.WaitGroup
+	stopCh := make(chan struct{})
+
+	tests := []struct {
+		desc         string
+		inReq        *mpb.StopRequest
+		inSessions   map[string]chan struct{}
+		inListenFunc func()
+		wantCode     codes.Code
+	}{{
+		desc:     "empty input",
+		inReq:    &mpb.StopRequest{},
+		wantCode: codes.InvalidArgument,
+	}, {
+		desc:     "no from",
+		inReq:    &mpb.StopRequest{To: "eth2"},
+		wantCode: codes.InvalidArgument,
+	}, {
+		desc:     "no to",
+		inReq:    &mpb.StopRequest{From: "eth3"},
+		wantCode: codes.InvalidArgument,
+	}, {
+		desc:       "session does not exist",
+		inReq:      &mpb.StopRequest{From: "eth42", To: "eth2"},
+		inSessions: map[string]chan struct{}{"eth1:eth2": make(chan struct{})},
+		wantCode:   codes.NotFound,
+	}, {
+		desc:       "create new session",
+		inReq:      &mpb.StopRequest{From: "eth1", To: "eth2"},
+		inSessions: map[string]chan struct{}{"eth1:eth2": stopCh},
+		inListenFunc: func() {
+			<-stopCh
+			called = true
+			wg.Done()
+			return
+		},
+	}}
+
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+
+			if tt.inListenFunc != nil {
+				wg.Add(1)
+				go tt.inListenFunc()
+			}
+
+			ms, addr := startServer(t)
+			conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+			if err != nil {
+				t.Fatalf("got error in grpc.Dial(%v), got err: %v", addr, err)
+			}
+
+			if tt.inSessions != nil {
+				ms.stopChs = tt.inSessions
+			}
+
+			client := mpb.NewMirrorClient(conn)
+			got, err := client.Stop(context.Background(), tt.inReq)
+			t.Logf("got: %s %v", got, err)
+			if err != nil {
+				s, ok := status.FromError(err)
+				if !ok {
+					t.Fatalf("did not get correct return type, %v", err)
+				}
+				if s.Code() != tt.wantCode {
+					t.Fatalf("did not get expected error code, got: %v, want: %v", s.Code(), tt.wantCode)
+				}
+			}
+
+			wg.Wait()
+			if tt.inListenFunc != nil {
+				t.Logf("checking for value of called, got: %v", called)
+				if called != true {
+					t.Fatalf("did not get expected called value, got: %v, want: true", called)
+				}
+			}
+
+			called = false
+		})
+	}
+}
