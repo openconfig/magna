30abb3e1d260f66275ee2781aefab1d559b4c6d6
Rob Shakir <robjs@google.com>
2023-07-20 20:22:03 -0700

    Add mirroring gRPC container [2/3] (#27)
    * working commit.
    
    * Update simple e2e ONDATRA test.
    
    * Update integration test.
    
    * Update mirror implementation.
    
    * Add a gRPC interface to control packet mirroring.
    
    * Refactor mirror service into a gRPC microservice.
    
     * (A) mirrorsrv/mirror(_test)?.go
       - small microservice that can be called from ONDATRA tests to
         allow for new mirroring sessions to be started and stopped.
     * (M) proto/mirror/...
       - add proto service definition.
    
    * Split change contents.
    
    * Remove 'replace' statement in go.mod
    
    * Fix lint.
    
    * Update comment per review comments.
    
    * Add integration test for e2e magna testing. [3/3]  (#28)
    
    * Add integration test for e2e magna testing.
    
    * Ensure test robustness.
    
    * Replace with fork of ONDATRA until PR is submitted.
    
    * Update to latest ONDATRA version.
    
    * Update CI testing workflow.
    
    * Avoid race in lwotg testing.
    
    * Address review comments.
    
    * Address review comments.

---
+++ cmd/mirror/main.go
@@ -0,0 +1,45 @@
+// Binary mirror implements a simple mechanism to copy packets that match
+// a filter between ports.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+	"sync"
+	"time"
+
+	"github.com/openconfig/magna/mirrorsrv"
+	"google.golang.org/grpc"
+	"k8s.io/klog/v2"
+
+	mpb "github.com/openconfig/magna/proto/mirror"
+)
+
+var (
+	port = flag.Uint("port", 60051, "port for mirror service to listen on")
+)
+
+func main() {
+	klog.InitFlags(nil)
+	flag.Parse()
+
+	srv := grpc.NewServer()
+	ms := mirrorsrv.New()
+	mpb.RegisterMirrorServer(srv, ms)
+
+	lis, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", *port))
+	if err != nil {
+		klog.Exitf("cannot start listening, got err: %v", err)
+	}
+
+	klog.Infof("mirror server listening on %s", lis.Addr().String())
+	defer srv.Stop()
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go srv.Serve(lis)
+	wg.Wait()
+	// Time to flush logs.
+	time.Sleep(2 * time.Second)
+}
